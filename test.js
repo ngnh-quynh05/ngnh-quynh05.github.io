825aa2342d7176210123a0b7317a014b820276e4
vudai2005 <90518082+vudai2005@users.noreply.github.com>
2022-01-15 16:15:46 +0700

    ww

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 alert('Chào mừng bạn đến với Website của tớ,chúc các bạn có 1 ngày tốt lành !.Thân ái ❤')
 var fullName;
-fullName = 'Xin chào tên tôi Vũ Trọng Đài';
+fullName = 'Xin chào tên tôi Nguyễn Như Quỳnh';
 alert(fullName);
 
